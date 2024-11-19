81595002723acb34fcbf9bac6844d6b5442938f9
MorPlatesMorDatez <dannycanale9@gmail.com>
2024-11-19 16:55:50 +0100

    Problema 2 versio A
    Signed-off-by: MorPlatesMorDatez <dannycanale9@gmail.com>

---
+++ Problem-2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        const string NUMEROS = "NUMEROS.txt";
+        StreamReader numerosTxt = new StreamReader(NUMEROS);
+        string linea = numerosTxt.ReadLine();
+        bool esParell = false;
+        int valor;
+        int contador = 0;
+        while(!(esParell || linea == null))
+        {
+            contador++;
+            valor = int.Parse(linea);
+            linea = numerosTxt.ReadLine();
+            if(valor %2 ==0)
+            {
+                esParell = true;
+            }
+        }
+        if(esParell) Console.WriteLine($"Numero parell trobat a la linea {contador}");
+        else Console.WriteLine("Cap numero es parell");
+    }
+}
