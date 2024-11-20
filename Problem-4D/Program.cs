4591f17bfc0ec95c7eb2708c61dc209ece456bc6
MorPlatesMorDatez <dannycanale9@gmail.com>
2024-11-20 12:36:05 +0100

    4-D i canvis al 4-C
    Signed-off-by: MorPlatesMorDatez <dannycanale9@gmail.com>

--- Problem-4D/Program.cs
+++ Problem-4D/Program.cs
@@ -6,18 +6,20 @@ internal class Program
     {
         const string PERFECTE = "PERFECTES.txt";
         StreamReader perfectesTxt = new StreamReader(PERFECTE);
-        string linea;
-        int valor = 0;
-
-        while ((linea = perfectesTxt.ReadLine()) != null)
+        string linea = perfectesTxt.ReadLine();
+        int valor;
+        bool esPerf = false;
+        while (!(esPerf||linea == null))
         {
             valor = int.Parse(linea);
-            bool esPerf = EsPerfecte(valor);
-            if (esPerf) Console.WriteLine($"{valor} Es perfecte");
-            else Console.WriteLine($"{valor} No es perfecte");
+            esPerf = EsPerfecte(valor);
+            if(esPerf) esPerf = true;
+            else linea = perfectesTxt.ReadLine();       
         }
-
-    }
+        if(esPerf) Console.WriteLine("Hi ha un valor perfecte");
+        else Console.WriteLine("No hi ha cap valor perfecte");
+    }   
+    
     public static bool EsPerfecte(int n)
     {
         int suma = 0;
