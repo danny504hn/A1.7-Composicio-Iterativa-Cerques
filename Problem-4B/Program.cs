03f5d442f6e7062cced30e92188088bba6d51eba
MorPlatesMorDatez <dannycanale9@gmail.com>
2024-11-20 11:33:35 +0100

    Problem 4B
    Signed-off-by: MorPlatesMorDatez <dannycanale9@gmail.com>

--- Problem-4B/Program.cs
+++ Problem-4B/Program.cs
@@ -1,13 +1,23 @@
 ﻿using System;
+using System.Diagnostics.Contracts;
 
 internal class Program
 {
     public static void Main(string[] args)
     {
-        
+        int n = 28;
+        bool esPerf = EsPerfecte(n);
+        Console.Write(esPerf);
     }
     public static bool EsPerfecte(int n)
     {
-        
+        int suma = 0;
+        int contador= 0;
+        while(contador < n/2)
+        {
+            contador++;
+            if(n % contador == 0) suma+=contador;
+        }
+        return suma == n;
     }
 }
