32f19c38b97ad0c0a9b0a00cbca1eebd6013d13e
MorPlatesMorDatez <dannycanale9@gmail.com>
2024-11-20 16:18:17 +0100

    -
    Signed-off-by: MorPlatesMorDatez <dannycanale9@gmail.com>

--- Problem-4/Program.cs
+++ Problem-4/Program.cs
@@ -7,14 +7,14 @@ internal class Program
         Console.Write("Introdueix un numero --> ");      
         int num = int.Parse(Console.ReadLine());
         int suma = 0;
+        int divisorMaxim = num / 2;
         int contador = 0;
-        while (contador <= num/2)
+        while (contador <= divisorMaxim && suma <num)
         {
             contador++;
             if (num % contador == 0) 
             {
             suma += contador;
-            Console.WriteLine($"{suma}  {contador}");
             }
         }
         if (num == suma) Console.WriteLine("Es perfecte");
