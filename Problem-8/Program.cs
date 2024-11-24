f4ebebc501e4d92924f72aaa0edd497632a95bf5
quepijemaje <danicana56@gmail.com>
2024-11-24 20:28:37 +0100

    PROBLEM-8

---
+++ Problem-8/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        const string FILE = "COMPLEIX.txt";
+        StreamReader file = new StreamReader(FILE);
+
+
+        string linea = file.ReadLine();
+        int suma = int.Parse(file.ReadLine());
+
+        int valor = 0;
+        bool trobat = false; 
+
+        while (!trobat && linea != null)
+        {
+            valor = int.Parse(linea);
+
+            if (valor == suma)
+            {
+                linea = file.ReadLine(); 
+                suma += valor;
+            }
+            else
+            trobat = valor != suma;
+             
+
+            
+        }
+        if (trobat) Console.WriteLine("NO");
+        else Console.WriteLine("SI");
+    }
+}
