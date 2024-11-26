9cc1f9829731497912c539e39d32536e7c53db71
MorPlatesMorDatez <dannycanale9@gmail.com>
2024-11-26 21:12:08 +0100

    problem-11
    Signed-off-by: MorPlatesMorDatez <dannycanale9@gmail.com>

---
+++ Problem-11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        const string FILE ="NOPERILL.TXT";       
+        StreamReader file = new StreamReader(FILE);
+        string linea = file.ReadLine();
+        int silurs;
+        int suma = 0;
+        bool trobat = false;
+        while(!trobat && linea!=null)
+        {
+            silurs = int.Parse(linea);
+            if(silurs !=0)
+            {
+                suma+=silurs;
+            }else if(suma <10)
+            {
+                trobat = true;
+            }
+            linea = file.ReadLine();
+        }
+        if(trobat) Console.WriteLine("EN PERILL");
+        else Console.WriteLine("NO ESTA EN PERILL");
+    }
+}
