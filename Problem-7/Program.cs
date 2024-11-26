0a11da1ae0bf1e551f95b0e851d017f95b3eecca
MorPlatesMorDatez <dannycanale9@gmail.com>
2024-11-26 21:25:30 +0100

    Problema-7
    Signed-off-by: MorPlatesMorDatez <dannycanale9@gmail.com>

---
+++ Problem-7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        const string FILE ="TROBAT.TXT";
+        StreamReader file = new StreamReader(FILE);
+        string linea = file.ReadLine();
+
+        int contador = 0;
+        int dorsal;
+        bool trobat = false;
+
+        while(!trobat && linea!=null)
+        {
+            contador++;
+            dorsal = int.Parse(linea);
+            if(dorsal == 231)
+            {
+                trobat = true;
+            }
+            linea = file.ReadLine();
+        }
+        if(trobat) Console.WriteLine($"Dorsal 231 arriba en la posicio {contador}");
+        else Console.WriteLine("Dorsal 231 s'ha perdut");
+    }
+}
