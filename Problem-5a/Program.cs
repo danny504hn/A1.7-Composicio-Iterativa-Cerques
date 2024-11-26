6217b48535caa962e07f784a17975fcc3afea8da
MorPlatesMorDatez <dannycanale9@gmail.com>
2024-11-26 22:07:51 +0100

    problem 5a
    Signed-off-by: MorPlatesMorDatez <dannycanale9@gmail.com>

---
+++ Problem-5a/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        double numero;
+        double divisors = 2;
+        bool esPrimer = true;
+        Console.Write("Introdueix un numero --> ");
+        numero = double.Parse(Console.ReadLine());
+        if (numero <= 1) esPrimer = true;
+        else
+        {
+            while (divisors <= Math.Sqrt(numero))
+            {
+                if (numero % divisors == 0)
+                {
+                    esPrimer = false;
+                }
+                divisors++;
+            }
+        }
+        if (esPrimer) Console.WriteLine("Es primer");
+        else Console.WriteLine("no es primer");
+    }
+}
