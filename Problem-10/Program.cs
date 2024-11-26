a1a343dee9c1aec6cf4782c66ad4d0b6f6b3cec2
MorPlatesMorDatez <dannycanale9@gmail.com>
2024-11-26 20:57:04 +0100

    Problem-10
    Signed-off-by: MorPlatesMorDatez <dannycanale9@gmail.com>

---
+++ Problem-10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        const string FILE = "PLAGA.TXT";
+        StreamReader file = new StreamReader(FILE);
+        string linea = file.ReadLine();
+        int contador = 0;
+        int exemplars = 0;
+        bool trobat = false;
+
+        while (!trobat && linea != null)
+        {
+            exemplars = int.Parse(linea);
+            
+            if (exemplars > 500)
+            {
+                contador++;
+                if (contador == 5) trobat = true;
+            }
+            else contador = 0;
+
+            linea = file.ReadLine();
+        }
+        if (trobat) Console.WriteLine($"HI HA PLAGA");
+        else Console.WriteLine($"NO HI HA PLAGA");
+    }
+}
