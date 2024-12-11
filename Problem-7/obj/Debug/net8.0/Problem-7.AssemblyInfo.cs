e6c0fc2b64b1b33ca0e5132592712d647e6e6811
danny504 <danicana56@gmail.com>
2024-12-11 22:12:34 +0100

    tot
    Signed-off-by: danny504 <danicana56@gmail.com>

--- Problem-7/obj/Debug/net8.0/Problem-7.AssemblyInfo.cs
+++ Problem-7/obj/Debug/net8.0/Problem-7.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Problem-7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6217b48535caa962e07f784a17975fcc3afea8da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7374c18c69f363da4ff6ef994ff3517a1c242f52")]
 [assembly: System.Reflection.AssemblyProductAttribute("Problem-7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Problem-7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
